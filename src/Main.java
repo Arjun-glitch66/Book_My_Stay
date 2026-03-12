75776a27493274c36d7401a06b32a2be4ba345e6
Arjun <arjunanand206@gmail.com>
2026-03-12 16:47:27 +0530

    UC4

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,53 @@
 
 import java.util.*;
-class roomInventory{
-  HashMap<String,Integer> roomAvailablity;
+class roomInventory {
+  HashMap<String, Integer> roomAvailablity;
 
-  roomInventory(){
+  roomInventory() {
     roomAvailablity = new HashMap<>();
     roomAvailablity.put("Single", 5);
-    roomAvailablity.put("Double",3);
-    roomAvailablity.put("Suite",2);
+    roomAvailablity.put("Double", 3);
+    roomAvailablity.put("Suite", 2);
   }
+  HashMap<String, Integer> getRoomAvailablity () {
+      return roomAvailablity;
+  }
+}
 
-  HashMap<String,Integer> getRoomAvailablity(){
-    return roomAvailablity;
+class roomSearchService{
+  void searchAvailableRooms(){
+    roomInventory ob1 = new roomInventory();
+    HashMap<String,Integer> availablity=ob1.getRoomAvailablity(); //Duplicate of main HashMap
+    for(String room:availablity.keySet()){ //Runs from 0th till end, keyset return key
+      if(availablity.get(room)>0) { // availablity.get("Single") returns the value =5 because availablity only has the key-value pair
+        if (room.equals("Single")) {
+          System.out.println("Single Room");
+          System.out.println("Beds: 1");
+          System.out.println("Size: 250 sqt");
+          System.out.println("Availble rooms: " + availablity.get(room));
+        }
+        else if (room.equals("Double")) {
+          System.out.println("Double Room");
+          System.out.println("Beds: 2");
+          System.out.println("Size: 500 sqt");
+          System.out.println("Availble rooms: " + availablity.get(room));
+        }
+        else if (room.equals("Suite")) {
+          System.out.println("Suite Room");
+          System.out.println("Beds: 3");
+          System.out.println("Size: 1000 sqt");
+          System.out.println("Availble rooms: " + availablity.get(room));
+        }
+      }
+
+    }
   }
 }
 public class Main {
   public static void main(String[] args) {
-    roomInventory ob= new roomInventory();
-    HashMap<String,Integer> store=ob.getRoomAvailablity();
-    for (String room : store.keySet(){
-      if (room.equals("Single")) {
-        System.out.println("Single Room");
-        System.out.println("Beds: 1");
-        System.out.println("Size: 250 sqt");
-        System.out.println("Availble rooms: " + store.get(room));
-      }
-    if (room.equals("Double")) {
-      System.out.println("Double Room");
-      System.out.println("Beds: 2");
-      System.out.println("Size: 500 sqt");
-      System.out.println("Availble rooms: " + store.get(room));
-    }
-    if (room.equals("Suite")) {
-      System.out.println("Suite Room");
-      System.out.println("Beds: 1");
-      System.out.println("Size: 1000 sqt");
-      System.out.println("Availble rooms: " + store.get(room));
-    }
+    roomSearchService ob2= new roomSearchService();
+    ob2.searchAvailableRooms();
   }
 }
-}
+
 
